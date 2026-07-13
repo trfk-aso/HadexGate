2b6bd3e17a57138e07828b9bd10716f4d7ec904f
Anna <masananuta@gmail.com>
2026-07-13 13:12:14 +0300

    Initial Commit

---
+++ HadexGate/HadexGateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HadexGateApp.swift
+//  HadexGate
+//
+//  Created by Анна Масан on 13.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct HadexGateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
